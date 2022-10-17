43eb8f33c9b38d9de37c9618f5f6fba88ab93ecc
@Juan-Pablo-Barrientos <jpbarrientosros@gmail.com>
2022-10-17 20:21:03 -0300

    Initial push

---
+++ index.js
@@ -0,0 +1,24 @@
+//require("./database/index");
+const express = require("express");
+const cors = require("cors");
+//const routes = require("./routes");
+const http = require("http");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const server = http.createServer(app);
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(`Server running on port ${port}`);
+});
+
+module.exports = app;
