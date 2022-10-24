e64d79db6195dc1fbfeca0be51c868812db617eb
@Juan-Pablo-Barrientos <jpbarrientosros@gmail.com>
2022-10-24 14:10:36 -0300

    mongoose and sequelize merged

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const cors = require("cors");
 const http = require("http");
+const db = require('./backend-mongoose/models/db.js');
 const app = express();
 const router = require("./src/sequelize/routes/index");
 require ("dotenv").config();
-const Associations = require('../POC-TTADS/src/sequelize/models/relations')();
-
+const Associations = require('./src/sequelize/models/relations')();
+const equipo = require('./backend-mongoose/models/equipo/equipoController.js')
 app.use(express.json());
 app.use(cors());
 app.use(router);
-
+app.use('/api/equipo', equipo);
 const server = http.createServer(app);
 
 const port = process.env.PORT || 3000;
